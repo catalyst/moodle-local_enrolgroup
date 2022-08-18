7652ca514d16c98a1e70dcb07932686f6000ff6d
Dan Marsden <dan@danmarsden.com>
2022-08-18 13:45:54 +1200

    Add cli script for running group sync.

---
+++ cli/sync_groups.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * 
+ * @package    local_enrolgroup
+ * @copyright  2022 Catalyst IT
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+define('CLI_SCRIPT', true);
+
+require(__DIR__.'/../../../config.php');
+require_once("$CFG->libdir/clilib.php");
+
+
+global $CFG;
+require_once($CFG->dirroot.'/enrol/database/lib.php');
+$trace = new \text_progress_trace();
+
+if (!enrol_is_enabled('database')) {
+    $trace->output('Plugin not enabled');
+    return;
+}
+
+
+$enrol = new local_enrolgroup\task\helper();
+$enrol->sync_groups($trace);
+$trace->finished();
